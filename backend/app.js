037211b04367b08d5c386c994f1918004b60f9b0
Nishant Yadav <nishanty736@gmail.com>
2026-06-28 15:02:35 +0530

    cors update

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://task-flow-alpha-sandy.vercel.app",
+    ],
     credentials: true,
   }),
 );
